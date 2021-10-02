1ea689edd55445de74ebb896f026a5164c898948
mukul-kc <mukul@karmacircles.com>
2021-10-02 18:06:14 +0530

    Add backend integration to frontend

--- client/src/Components/UserPage/UserPage.jsx
+++ client/src/Components/UserPage/UserPage.jsx
@@ -4,17 +4,19 @@ import { Button } from "react-bootstrap";
 import './UserPage.css';
 
 const UserPage = (props) => {
-    const [companyName, setCompanyName] = useState(props.location.pathname)
+    const [companyID, setCompanyID] = useState(props.location.pathname)
     const [email, setEmail] = useState("");
     const [warning, setWarning] = useState("");
     const [showOTP, setShowOTP] = useState(false);
     const [OTP, setOTP] = useState("");
+    const [name, setName] = useState("");
+    const [message, setMessage] = useState("");
 
     useEffect(() => {
         const path = props.location.pathname;
-        setCompanyName(path.substr(1,path.length));
+        setCompanyID(path.substr(1, path.length));
     }, [])
- 
+
     function validateEmail(email) {
         const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(String(email).toLowerCase());
@@ -22,32 +24,34 @@ const UserPage = (props) => {
 
     async function onSubmit(e) {
         const isOkay = validateEmail(email);
-        if(!isOkay) {
+        if (!isOkay) {
             setWarning("Please enter a valid email address")
         }
         else {
             // send to server, show OTP box
-                try {
-                const response = await axios.post("localhost:8000/signup", {email: email});
-                setWarning(response.message); // OTP has been sent or something went wrong
-                if(response.status === 200) {
+            try {
+                const response = await axios.get('http://localhost:8000/api/signup', { params: { email: email, name: name, user_id: companyID } });
+                console.log(response);
+                if (response.status === 200) {
                     // proceed to verify user 
+                    setMessage(response.data);
                     setShowOTP(true);
                 }
+                else setWarning(response.data);
             }
-            catch(e) {
-                setWarning("Something went wrong");
+            catch (e) {
+                setWarning(e.response.error);
             }
         }
-    }           
+    }
 
     async function verifyOTP() {
         try {
-            const response = await axios.post('localhost:8000/verify_user', {email: email, otp: OTP});
-            if(response.status === 200) setWarning(response)
+            const response = await axios.post('localhost:8000/api/signup', { email: email, user_id: companyID, otp: OTP });
+            if (response.status === 200) setWarning(response)
             else setWarning("Something went wrong");
         }
-        catch(e) {
+        catch (e) {
             setWarning("Something went wrong");
         }
     }
@@ -57,19 +61,26 @@ const UserPage = (props) => {
             <div className='details-box'>
                 Subscribe to our feature updates!
                 {!showOTP && <div id='input-field'>
-                    <input
-                        type="text"
-                        value = {email}
-                        placeholder='Enter your email'
-                        onChange = {(e) => setEmail(e.target.value)}                
-                    />
-                    <div id='warning'>
-                        {warning}
+                    <div>
+                        <input
+                            type="text"
+                            value={email}
+                            placeholder='Enter your email'
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+
+                    </div>
+                    <div id='name-field'>
+                        <input type="text" value={name} placeholder='Your name' onChange={(e) => setName(e.target.value)} />
                     </div>
                 </div>}
-                {showOTP && <div id='input-field'><input placeholder='Enter OTP' type="text" value={OTP} onChange={(e) => setOTP(e.target.value)}/></div>}
-                {showOTP && <Button onClick = {verifyOTP}>Verify</Button>}
-                {!showOTP && <Button onClick = {onSubmit}>Submit!</Button>}
+                {showOTP && <div id='input-field'><input placeholder='Enter OTP' type="text" value={OTP} onChange={(e) => setOTP(e.target.value)} /></div>}
+                {showOTP && <Button onClick={verifyOTP}>Verify</Button>}
+                <div id='warning'>
+                    {warning}
+                </div>
+                {message}
+                {!showOTP && <Button onClick={onSubmit}>Submit!</Button>}
             </div>
         </div>
     );
