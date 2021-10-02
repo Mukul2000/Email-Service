26590af8cafbae03040287f850ff4513cd16ec38
mukul-kc <mukul@karmacircles.com>
2021-10-02 21:38:11 +0530

    Add jwt utils and auth middleware

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const route = express.Router();
-const signup_controllers = require('../controllers/signup')
+const signup_controllers = require('../controllers/signup');
+const login_controllers = require('../controllers/login')
 
 route.get('/signup', signup_controllers.signup_user);
 route.post('/signup',signup_controllers.verify_user);
+route.delete('/signup', signup_controllers.delete_user);
+
+route.post('/login', login_controllers.login)
 
 module.exports = route;
