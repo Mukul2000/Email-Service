ecd5b6f04d67e99c599381b5ed8223282664eb8f
mukul-kc <mukul@karmacircles.com>
2021-10-23 13:11:22 +0530

    Fix dashboard UI

--- client/src/Components/Dashboard/Dashboard.jsx
+++ client/src/Components/Dashboard/Dashboard.jsx
@@ -4,28 +4,46 @@ import NavigationBar from "../NavigationBar/NavigationBar";
 import './Dashboard.css';
 
 function Dashboard(props) {
-    const tables = [ ['Subscribers', 'subscribers'], ['History', 'history'], ['Scheduled', 'scheduled'], ['Templates', 'templates']]
-    // const history = useHistory();
+    const [name, setName] = useState("");
+    const [activeTab, setActiveTab] = useState(0);
 
-    // useEffect(() => {
-    //     const user = localStorage.getItem('user');
-    //     if(!user) history.push('/login');
-    // });
+
+    const tables = [['Subscribers', 'subscribers', 'fas fa-home'], ['History', 'history', 'fas fa-history'],
+    ['Scheduled', 'scheduled', 'fas fa-calendar-alt'], ['Templates', 'templates', 'fas fa-book-open']];
+
+    useEffect(() => {
+        const name = JSON.parse(localStorage.getItem('user')).name;
+        setName(name);
+    }, [])
 
     return (
         <div className='outer-wrapper'>
             <NavigationBar />
             <div className='content-wrapper'>
-                <h3> User Data </h3>
-                {tables.map((item, index) => {
-                    return (
-                        <div className='table-link' key={index}>
-                            <Link to={{
-                                pathname: `/user_tables/${item[1]}`,
-                            }} > {item[0]}</Link>
-                        </div>
-                    )
-                })}
+                <div className='tabs-list'>
+                    <div className='greeting'>
+                        <div className='welcome-text'>Hello {name}</div>
+                        <div className='subtitle'> Welcome back! </div>
+                    </div>
+
+                    <div id='space-box'></div>
+                    {tables.map((item, index) => {
+                        return (
+                            <div className='table-link' key={index} onClick={() => setActiveTab(index)}>
+                                <i className={item[2]}></i>
+                                <span className='table-name'>
+                                    {item[0]}
+                                </span>
+                                {/* <Link to={{
+                                    pathname: `/user_tables/${item[1]}`,
+                                }} > {item[0]}</Link> */}
+                            </div>
+                        )
+                    })}
+                </div>
+                <div className='tab-data'>
+                    {activeTab}
+                </div>
             </div>
         </div>
     )
